2b7d4b58bd9e1d9945bf033bfb7d266cb16cd822
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2022-06-28 22:29:24 +0900

    solve a prob

--- Baekjoon/다익스트라/1504_특정한_최단_경로/1504.py
+++ Baekjoon/다익스트라/1504_특정한_최단_경로/1504.py
@@ -1,38 +1,39 @@
 import sys, heapq
 input = sys.stdin.readline
 
+def do(v):
+    visited = [0] * (V+1)
+    dist = [max_v] * (V+1)
+    heap = [(0, v)]
+    while heap:
+        w, v = heapq.heappop(heap)
+        if not visited[v]:
+            visited[v] = 1
+            dist[v] = w
+            for nod, weight in g[v]:
+                if not visited[nod] and dist[nod] > w + weight:
+                    heapq.heappush(heap, (w + weight, nod))
+    if visited[v1] and visited[v2] and visited[V]:
+        return dist
+    else:
+        return False
+
+
+max_v = sys.maxsize
 V, E = map(int, input().strip().split())
-g = [[] for _ in range(V+1)]
+g = [[] for _ in range (V+1)]
 
-
-for i in range(E):
+for _ in range(E):
     start, end, weight = map(int, input().strip().split())
     g[start].append((end, weight))
     g[end].append((start, weight))
 
-u, v = map(int, input().strip().split())
+v1, v2 = map(int, input().strip().split())
 
-def do(start):
-    visited = [0] * (V+1)
-    dist = [sys.maxsize] * (V+1)
-    heap = [(0, start)]
-    while heap:
-        w, node = heapq.heappop(heap)
-        if not visited[node]:
-            visited[node] = 1
-            dist[node] = w
-            for i in g[node]:
-                if not visited[i][0] and dist[i][0] > w + g[node][i][1]:
-                    heapq.heappush(heap, (w + g[node][i], i))
-
-
-    return visited
-
-S = do(1)
-U = do(u)
-V = do(v)
-
-# 경우의 수 2가지
-# 더 적게 드는 것 구하자
-# 1 => u => v => N
-# 1 => v => u => N
+array1 = do(1)
+if array1:
+    array2 = do(v1)
+    array3 = do(v2)
+    print(min(array1[v1]+array2[v2]+array3[V], array1[v2]+array3[v1]+array2[V]))
+else:
+    print(-1)
